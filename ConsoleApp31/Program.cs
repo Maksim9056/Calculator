b4726c1a176efa4b27e0eea010514c5f0ec1bb59
Maksim <bobreczovm@inbox.ru>
2023-10-26 10:38:22 +0300

    Calculator

--- ConsoleApp31/Program.cs
+++ ConsoleApp31/Program.cs
@@ -2,13 +2,20 @@
 {
     internal class Program
     {
+
+        /// <summary>
+        /// Напишите обобщенный класс Calculator<T>,
+        /// который имеет статический метод T Add(T x, T y), который возвращает сумму двух значений типа T.
+        /// Затем напишите ограничение для этого класса, чтобы он мог работать только с типами,
+        /// которые имеют параметрless конструктор (new()). 
+        /// Затем напишите метод T Zero(), который возвращает нулевое значение типа T с помощью параметрless конструктора.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Calculator<int> calculator = new ConsoleApp31.Calculator<int>();
 
            var Values =   calculator.Add(10, 20);
-
-
             foreach (var value in Values)
             {
                 Console.WriteLine(value);
