da6979301783a42e353bef3c14ebb72b0a01d29d
Maksim <bobreczovm@inbox.ru>
2023-10-26 10:35:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp31/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    public class Calculator<T>
+    {
+        
+
+        public  Calculator()
+        {
+        }
+
+        public T X { get; set; }
+        public T Y { get; set; }
+        public T[]  Add(T x, T y)
+        {
+
+            X = x;
+            Y = y;
+
+            T[] values = new T[2];
+
+            values[0] = X; 
+            values[1] = Y;
+            return values;
+        }
+
+        public T Zero<T>()
+        {
+            return default(T);
+        }
+    }
+}
---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator<int> calculator = new ConsoleApp31.Calculator<int>();
+
+           var Values =   calculator.Add(10, 20);
+
+
+            foreach (var value in Values)
+            {
+                Console.WriteLine(value);
+            }
+
+
+            var Value = calculator.Zero<int>();
+
+            Console.WriteLine(Value);
+
+            Console.ReadLine();
+        }
+
+    } 
+}
+    
